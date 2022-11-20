ff59cce865ce85333b0ef735a446350174cae931
DavidDov <109440152+Daviddov@users.noreply.github.com>
2022-11-20 17:25:06 +0200

    u

--- node/httpServer/index.js
+++ node/httpServer/index.js
@@ -57,26 +57,44 @@ const server = http.createServer((req, res) => {
                 }
                 break;
 
-            case '/contacts':
-                fs.readFile('./files/contacts.json', function (err, data) {
-                    res.writeHead(200, { 'Content-Type': 'text/json' });
-                    res.write(data);
-                    return res.end();
-                });
-                break;
+            case 'contacts':
+                if (urlSplit[2]) {
+                    fs.readFile('./files/contacts.json', function (err, data) {
+                        const dataParse = JSON.parse(data)
+                        const dataStringify = JSON.stringify(dataParse[urlSplit[2]])
 
-            case '/contacts/2':
-                fs.readFile('./files/contacts.json', function (err, data) {
-                    res.writeHead(200, { 'Content-Type': 'text/json' });
-                    res.write(data);
-                    return res.end();
-                });
+                        if (urlSplit[2] >= dataParse.length) {
+                            console.log('data not Exists');
+                            return res.end();
+
+                        } res.writeHead(200, { 'Content-Type': 'text/json' });
+                        res.write(dataStringify);
+                        return res.end();
+                    });
+                } else {
+                    fs.readFile('./files/contacts.json', function (err, data) {
+                        res.writeHead(200, { 'Content-Type': 'text/json' });
+                        res.write(data);
+                        return res.end();
+                    });
+                }
+                break;
+                case 'comps':
+                    fs.readFile('./files/comps.html', function (err, data) {
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.write(data);
+                        return res.end();
+                    })
                 break;
             default:
                 console.log(url, urlSplit);
+                fs.readFile('./files/error.html', function (err, data) {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(data);
+                    return res.end();
+                });
                 break;
         }
-
     }
 
 })
